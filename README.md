78f567963a0862c36a269df846a950cc508c468e
Gabriel Costa <gabriel.prates.costa@gmail.com>
2023-08-22 11:09:16 -0300

    Repeatedly click the 'connect' button

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# LinkedIinBot
+
+# LinkedIn bot
+
+A bot to automatically follow people on the LinkedIn social network.
+## Authors
+
+- [@GARGA342](https://github.com/GARGA342)
+
--- main.py
+++ main.py
@@ -1,79 +1,82 @@
-class Bot:
-    from selenium import webdriver
-    from time import sleep
-    from random import randint
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+from time import sleep
+from random import randint
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.remote.webelement import WebElement
 
+class Bot():
     def __init__(self):
         #Chose driver to browse
-        options = self.Options()
+        options = Options()
         options.add_argument("executable_path=/home/garga/Projects/Python/LinkedInBot/geckodriver")
 
         #Define webdriver
-        self.driver = self.webdriver.Firefox(options=options)
+        self.driver = webdriver.Firefox(options=options)
 
-    def getPage(self, page):
+    def getPage(self, page = ""):
         try:
             #Find page in browser
             self.driver.get(page)
-            self.sleep(self.randint(1, 4))
-        except:
+            sleep(randint(1, 4))
+        except:    
             print("Não foi possível acessar o site")
-            exit(1)
+            exit(1) 
 
-    def login(self):
+    def login():
         print(f'\n\nSeu login deve ser feito manualmente. Assim que logar, tecle ENTER.\n\n')
         input()
-        self.sleep()
+        sleep(randint(1, 4))
 
-    def findElement(self, elem):
+    def findElement(self, elem = "") -> any:
         try:
             #Find element in page and returns it
-            return self.clickElement(self.driver.find_element(self.By.XPATH, elem))
+            return self.driver.find_element(By.XPATH, elem)
+        
         except:
             print(f'Não foi possível encontrar o elemento \n\t{elem}')
-            exit(1)
+            return False
 
-    def clickElement(self, elem):
+    def clickElement(elem) -> bool:
         try:
-            #########################################################################
-            pass
-        except:
-            print(f'Não foi possível acessar o elemento \n\t{elem}')
-            exit(1)
+            elem.click()
+            sleep(randint(1, 4))
+            return True
+        except:                
+            return False
+
+    def refreshPage(self):
+            self.driver.refresh
+            sleep(randint(1, 4))
+
+bot = Bot()
 
 #Open website -> "https://br.linkedin.com/"
-linkedin = Bot()
-linkedin.getPage("https://br.linkedin.com/")
+bot.getPage("https://br.linkedin.com/?original_referer=")
 
-#Enter login
-linkedin.login()
+#Enter login manually
+bot.login()
 
 #Element find
-elem = linkedin.findElement("/html/body/div[5]/header/div/nav/ul/li[2]")
+elem = bot.findElement("/html/body/div[5]/header/div/nav/ul/li[2]")
 
 #Element click
-#linkedin.clickElement(elem)
+bot.clickElement(elem)
 
-#    #Find element and click
-#    elem = driver.find_element(By.XPATH, '/html/body/div[5]/header/div/nav/ul/li[2]')
-#    elem.click()
-#
-#    #Wait 4 seconds and scroll page
-#    sleep(4)
-#    driver.execute_script("window.scrollTo(0, 2300);")
-#
-#    #Wait 2 seconds and repeatedly click the "connect" button
-#    sleep(2)
-#
-#    while(True):
-#        elem = driver.find_element(By.XPATH, "/html/body/div[5]/div[3]/div/div/div/div/div[2]/div/div/main/ul/li[1]/ul/li[1]/div/section/div[3]/footer/button/span")
-#        elem.click()
-#        sleep(1)
-#        try:
-#            elem = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[3]/button[1]/span")
-#            elem.click()
-#            sleep(1)
-#        except:
-#            None        
+#Find element and click
+bot.clickElement(bot.findElement("/html/body/div[5]/header/div/nav/ul/li[2]"))
+
+#Scroll page
+bot.scrollPage()
+
+i=1
+while(True):
+    #Repeatedly click the "connect" button
+    elem = bot.findElement(f'/html/body/div[5]/div[3]/div/div/div/div/div[2]/div/div/main/ul/li[1]/ul/li[{i}]/div/section/div[3]/footer/button/span')
+                    
+    if isinstance(elem, WebElement):
+        bot.clickElement(elem)
+        i+=1
+    else:
+        bot.refreshPage()
+        i=1
